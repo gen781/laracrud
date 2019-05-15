33bc218985120e20c7ef6e90d34ec9e575fc2d43
Rahmat Ade Saputra <gen781@googlemail.com>
2019-05-15 21:53:03 +0700

    update

---
+++ database/migrations/2019_05_15_144226_create_customers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama_customer');
+            $table->string('alamat');
+            $table->timestamps('tgl_masuk');
+            $table->integer('limit');
+            $table->string('no_ktp')->unique();
+            $table->integer('operator');
+            $table->string('no_rek');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
